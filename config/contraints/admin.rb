5ef7a930524ad157624a54f8c6d002d845919e28
Claderoki <49477117+claderoki@users.noreply.github.com>
2021-07-09 12:11:34 +0200

    added admin

--- config/contraints/admin.rb
+++
@@ -1,9 +0,0 @@
-class AdminConstraint
-    ADMIN_USER_IDS = [120566758091259906, 247855177074212865, 841255759978954763]
-
-    def matches?(request)
-      me = OauthController::get_me(request.session)
-      
-      return me != nil && ADMIN_USER_IDS.include?(Integer(me['id']))
-    end
-  end
